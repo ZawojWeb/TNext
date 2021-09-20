9b14686dae9545bee55a215ccd12948c4cab6d4f
Kacper Zawojski <zawojweb@gmail.com>
2021-09-20 15:35:44 +0200

    getStaticProps

--- dj-events-front/pages/index.js
+++ dj-events-front/pages/index.js
@@ -1,11 +1,24 @@
-
 import Link from 'next/link'
 import { Layout } from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({events}) {
+console.log(events);
   return (
     <Layout>
       <h1>Home</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  console.log(events);
+
+  return {
+    props: {events},
+    revalidate: 1,
+  }
 }
