ba0972ba635373ab2fc27370f30c8a2c5d7581bc
Kacper Zawojski <zawojweb@gmail.com>
2021-09-28 15:31:28 +0200

    Store JWT in Server HttpOnly Cookie

--- dj-events-front/components/Layout.js
+++ dj-events-front/components/Layout.js
@@ -11,8 +11,8 @@ export const Layout = ({title,keywords,description, children}) => {
     return (
         <>
             <Head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>{title}</title>
                 <meta name="description" content={description}/>
--- dj-events-front/pages/api/login.js
+++ dj-events-front/pages/api/login.js
@@ -1,3 +1,4 @@
+import cookie from 'cookie'
 import { API_URL } from "@/config/index";
 
 
@@ -20,10 +21,17 @@ export default async (req,res) => {
         const data = await strapiRes.json()
         
         if(strapiRes.ok){
-            //* TODO Set the cookie
+            // Set cookie
+            res.setHeader('Set-Cookie', cookie.serialize('token', data.jwt, {
+                httpOnly:true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24 * 7, // One week
+                sameSite: 'strict',
+                path : '/'
+            }))
+
             res.status(200).json({user:data.user})
         }else{
-            console.log(password , identifier);
             res.status(data.statusCode).json({message: data.message[0].messages[0].message})
         }
 
