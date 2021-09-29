5b85a1ae5019ec9bb8befa1339c825c9f918adaa
Kacper Zawojski <zawojweb@gmail.com>
2021-09-29 17:44:45 +0200

    Getting user's events

--- dj-events-backend/api/events/controllers/events.js
+++ dj-events-backend/api/events/controllers/events.js
@@ -10,17 +10,16 @@ module.exports = {
 
     async me(ctx){
         const user = ctx.state.user
-
+        
         if(!user){
             return ctx.badRequest(null, [{messages: [{id: 'No authorization header  was found'}]}])
         }
-
+        
         const data = await strapi.services.events.find({user: user.id})
-
+        
         if(!data){
             return ctx.notFound()
         }
-
-        return sanitizeEntity(data, {module: strapi.modules.events})
+        return sanitizeEntity(data, {model: strapi.models.events})
     }
 };
--- dj-events-front/pages/account/dashboard.js
+++ dj-events-front/pages/account/dashboard.js
@@ -1,6 +1,9 @@
 import { Layout } from "@/components/Layout"
+import { API_URL } from "@/config/index"
+import { parseCookies } from "@/helpers/index"
 
-const DashboardPage = () => {
+const DashboardPage = ({events}) => {
+    console.log(events);
     return (
         <Layout title="User Dashboard">
             <h1>Dashboard</h1>
@@ -9,3 +12,22 @@ const DashboardPage = () => {
 }
 
 export default DashboardPage
+
+
+export async function getServerSideProps({req}){
+    const {token} = parseCookies(req)
+
+    const res = await fetch(`${API_URL}/events/me`, {
+        method: "GET",
+        headers:{
+            Authorization: `Bearer ${token}`,
+        }
+    })
+    const events = await res.json();
+    
+    return {
+        props:{
+            events
+        },
+    }
+}
